48f831054a849d68a4967f80512fc8637ee20d55
mohsu <maureen.hsu@gmail.com>
2022-06-09 17:29:26 +0800

    change user to generic

--- mac.sh
+++ mac.sh
@@ -1,9 +1,11 @@
 # Install Command Line Tools
 xcode-select --install
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/mo/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Install Homebrew-cask
 brew install homebrew/cask
 
@@ -12,7 +14,8 @@ brew install --cask iterm2 sublime-text google-chrome firefox docker notion slac
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# Install powerlevel10k
+
+# Install plugin for oh-my-zsh: powerlevel10k, autosuggestions, highlights
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
